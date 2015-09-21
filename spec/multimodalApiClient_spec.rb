038547d0c3b9ae8b7ac5062b1ea1c7e979c633b7
François Breard <francois.breard@systrangroup.com>
2015-09-21 10:08:48 -0700

    Read Api key from file

--- spec/multimodalApiClient_spec.rb
+++ spec/multimodalApiClient_spec.rb
@@ -4,13 +4,24 @@ require_relative '../lib/multimodalApiClient'
 describe "MultimodalApiClient" do
   before :all do
     MultimodalApiClient::Swagger.configure do |configuration|
-      configuration.key = "<API KEY HEREs>"
+      configuration.host = "﻿https://platform.systran.net:8904"
+      if File.exists?("./key.txt")
+        key = File.read("./key.txt", :encoding => 'UTF-8')
+        if key.length > 0
+          configuration.key = key
+        else
+          puts "The key.txt file is empty"
+        end
+      else
+        puts"The key.txt file doesn't exists"
+      end
+
     end
 
   end
   describe "Configuration" do
     it "assures the user has a correct client configuration" do
-      expect(MultimodalApiClient::Swagger.configuration.key.length).to eq(36)
+      expect(MultimodalApiClient::Swagger.configuration.key.length).to be_between(10, 100)
     end
   end
   describe "MultimodalApi" do
